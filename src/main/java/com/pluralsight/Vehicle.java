d253510add2fc8d79fff57356b6edbd200dfbc14
ezrateshale <ezra.teshale12@gmail.com>
2026-04-15 20:21:45 -0500

    feat: Progress menu

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,97 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Vehicle {
+    private long vehicleId;
+    private String makeModel;
+    private String color;
+   private  int odometerReading;
+    private float price;
+    Scanner input=new Scanner(System.in);
+    public Vehicle(){
+        this.vehicleId = 0;
+        this.makeModel = "";
+        this.color = "";
+        this.odometerReading = 0;
+        this.price = 0.0f;
+    }
+    public Vehicle(long vehicleId, String makeModel, String color, int odometerReading, float price){
+        this.vehicleId=vehicleId;
+        this.makeModel=makeModel;
+        this.color=color;
+        this.odometerReading=odometerReading;
+        this.price=price;
+    }
+    public  static void displayAllVehicles(Vehicle[] vehicles) {
+        System.out.println("=====All vehicles======");
+        for (Vehicle v : vehicles) {
+            if (v != null) {
+                System.out.println("ID: " + v.getVehicleId() + ", Model: " + v.getMakeModel() + ", Color: " + v.getColor() + ", Odometer: " + v.getOdometerReading() + ", Price: $" + v.getPrice());
+            }
+        }
+    }
+public void addAVehicle(Vehicle[] vehicles){
+prompt(input,vehicles);
+}
+    public long getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(long vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getOdometerReading() {
+        return odometerReading;
+    }
+
+    public void setOdometerReading(int odometerReading) {
+        this.odometerReading = odometerReading;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    public void prompt(Scanner input,Vehicle[] vehicles){
+        System.out.println("What is the vehicleId?: ");
+        this.setVehicleId(input.nextLong());
+
+        input.nextLine();
+        System.out.print("Enter Make/Model: ");
+        this.setMakeModel(input.nextLine());
+
+        // Get color from user
+        System.out.print("Enter Color: ");
+        this.setColor(input.nextLine());
+
+        // Get odometer reading from user
+        System.out.print("Enter Odometer Reading: ");
+       this.setOdometerReading(input.nextInt());
+
+        // Get price from user
+        System.out.print("Enter Price: $");
+        this.setPrice(input.nextFloat());
+        System.out.println();
+    }
 }
--- src/main/java/com/pluralsight/VehicleInventoryApp.java
+++ src/main/java/com/pluralsight/VehicleInventoryApp.java
@@ -1,4 +1,62 @@
 package com.pluralsight;
-
+import java.util.Scanner;
 public class VehicleInventoryApp {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        Vehicle[] vehicles = new Vehicle[20];
+        vehicles[0] = new Vehicle(101121, "Ford Explorer", "Red", 45000, 13500);
+        vehicles[1] = new Vehicle(101122, "Toyota Camry", "Blue", 60000, 11000.00f);
+        vehicles[2] = new Vehicle(101123, "Chevrolet Malibu", "Black", 50000, 9700.00f);
+        vehicles[3] = new Vehicle(101124, "Honda Civic", "White", 70000, 7500.00f);
+        vehicles[4] = new Vehicle(101125, "Subaru Outback", "Green", 55000, 14500.00f);
+        vehicles[5] = new Vehicle(101126, "Jeep Wrangler", "Yellow", 30000, 16000.00f);
+        //vehicles[0].displayAllVehicles(vehicles);
+        int counter = vehicles.length;
+        System.out.println(counter);
+        int option = menu(input);
+        System.out.println(option);
+        while(option==1||option==2||option==3||option==4||option==5){
+            if(option==1) {
+                Vehicle.displayAllVehicles(vehicles);
+                   // listAllVehicles();
+                    }
+           else if (option==2) {
+                    //findVehiclesByPrice();
+            }
+            else if (option==3) {
+            }
+            else if(option==4){
+                    }
+            else if(option==5){
+                Vehicle newVehicle = new Vehicle(0, "", "", 0, 0.0f);
+                newVehicle.addAVehicle(vehicles);
+                for(int i = 0; i < vehicles.length; i++) {
+                    if(vehicles[i] == null) {
+                        vehicles[i] = newVehicle;
+                        break;
+                    }
+
+                    // addAVehicle();
+            }
+            }
+            else{
+                break;
+            }
+            option=menu(input);
+        }
+        //System.out.println(option);
+        input.close();
+    }
+    public static int menu(Scanner input){
+        System.out.println("What do you want to do?");
+        System.out.println(" 1 - List all vehicles");
+        System.out.println(" 2 - Search by make/model");
+        System.out.println(" 3 - Search by price range");
+        System.out.println(" 4 - Search by color");
+        System.out.println(" 5 - Add a vehicle");
+        System.out.println(" 6 - Quit");
+        System.out.println("Enter your command: ");
+        int command = input.nextInt();
+        return command;
+    }
 }
